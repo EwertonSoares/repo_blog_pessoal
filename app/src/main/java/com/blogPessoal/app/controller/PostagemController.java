fe11e19089c1cc65c5d28ae15a357e8eaf0985e6
EwertonSoares <ewerton.fa4@gmail.com>
2023-01-30 13:57:08 -0300

    Ajustes

--- app/src/main/java/com/blogPessoal/app/controller/PostagemController.java
+++ app/src/main/java/com/blogPessoal/app/controller/PostagemController.java
@@ -4,20 +4,24 @@ import com.blogPessoal.app.model.Postagem;
 import com.blogPessoal.app.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PostagemController {
     @Autowired
-    private PostagemRepository repository;
+    private PostagemRepository postagemRepository;
 
-    @GetMapping("/buscar")
+    @GetMapping
     public ResponseEntity<List<Postagem>> getAll() {
 
-        return ResponseEntity.ok().body(repository.findAll());
+        return ResponseEntity.ok(postagemRepository.findAll());
 
     }
 }
