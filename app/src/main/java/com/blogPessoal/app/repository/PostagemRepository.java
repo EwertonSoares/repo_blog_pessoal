acfcedad3b1eb7370ccbd206a732a2d66bea0e0f
EwertonSoares <ewerton.fa4@gmail.com>
2023-01-31 10:40:03 -0300

    Adicinado novos metodos para PostagemController

--- app/src/main/java/com/blogPessoal/app/repository/PostagemRepository.java
+++ app/src/main/java/com/blogPessoal/app/repository/PostagemRepository.java
@@ -2,8 +2,14 @@ package com.blogPessoal.app.repository;
 
 import com.blogPessoal.app.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+    List<Postagem> findAllByTituloContainingIgnoreCase(@Param("Titulo") String titulo);
+//    Postagem findByTitulo(String titulo);
 }
